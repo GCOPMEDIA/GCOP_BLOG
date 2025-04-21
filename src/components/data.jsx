21a40a6e8df48df2f3081efae1bb03dbc7ec3420
Kobby24 <geffah005@st.ug.edu.gh>
2025-04-21 21:51:46 +0000

    feat: implement BlogList component to fetch and display posts from API

---
+++ src/components/data.jsx
@@ -0,0 +1,39 @@
+import axios from "axios";
+import React, { useEffect,useState } from "react";
+
+const BlogList = () => {
+    const [posts, setPosts] = useState([]);
+  
+    useEffect(() => {
+      axios.get('http://http://127.0.0.1:8000/all-posts/')
+        .then(response => {
+            
+          setPosts(response);
+        })
+        .catch(error => {
+          console.error("Error fetching posts:", error);
+        });
+    }, []);
+return posts}
+
+
+//     { id: 1,title: "Faith in Action", subtitle: "A powerful message of hope", url: "/article/1", media: "/images/sermon.jpg",
+//       like_count: 5 },
+//     { id: 2,title: "Youth Night Recap", subtitle: "Our vibrant youth in worship", url: "/article/2", media: "/images/youth.jpg",
+//       like_count: 4 },
+//     { id: 3,title: "Outreach Mission", subtitle: "Touching lives in our city", url: "/article/3", media: "/images/outreach.jpg" ,
+//       like_count: 45},
+//     {
+//       id: 4,
+//       title: "Blog Title",
+//       subtitle: "Optional subtitle",
+//       media: "https://your.image.url",
+//       video_url: "IkoIKcuq3ic", // if there's a video
+//       url: "/article/4" // where to navigate
+//       ,
+//       like_count: 5
+//     }
+//   ];
+// 
+
+  export default BlogList
