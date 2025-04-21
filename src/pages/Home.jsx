0cbf44ce82641a045be18b1adc798c833d695e3c
Kobby24 <geffah005@st.ug.edu.gh>
2025-04-21 21:52:00 +0000

    refactor: remove hardcoded blog cards and use BlogList for dynamic data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import Slider from "react-slick";
-import { Box, Card, CardContent, Typography, CardActionArea } from "@mui/material";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import { Box, Typography } from "@mui/material";
 import BlogGrid from "../components/Cards";
+import BlogList from "../components/data";
 
 const carouselImages = [
   "/images/church1.jpg",
@@ -11,24 +10,7 @@ const carouselImages = [
   "/images/church3.jpg"
 ];
 
-const blogCards = [
-  { id: 1,title: "Faith in Action", subtitle: "A powerful message of hope", url: "/article/1", media: "/images/sermon.jpg",
-    like_count: 5 },
-  { id: 2,title: "Youth Night Recap", subtitle: "Our vibrant youth in worship", url: "/article/2", media: "/images/youth.jpg",
-    like_count: 4 },
-  { id: 3,title: "Outreach Mission", subtitle: "Touching lives in our city", url: "/article/3", media: "/images/outreach.jpg" ,
-    like_count: 45},
-  {
-    id: 4,
-    title: "Blog Title",
-    subtitle: "Optional subtitle",
-    media: "https://your.image.url",
-    video_url: "IkoIKcuq3ic", // if there's a video
-    url: "/article/12" // where to navigate
-    ,
-    like_count: 5
-  }
-];
+
 
 const Home = () => {
   const carouselSettings = {
@@ -78,7 +60,7 @@ const Home = () => {
       </Box>
 
       {/* Blog Cards */}
-      <BlogGrid blogCards={blogCards}></BlogGrid>
+      <BlogGrid blogCards={BlogList}></BlogGrid>
     </Box>
     
     </main>
