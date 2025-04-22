457413a3297cf8d6be521030aa316ddc67088f69
Kobby24 <geffah005@st.ug.edu.gh>
2025-04-22 16:26:18 +0000

    feat: add authentication utility functions for token and username management

---
+++ src/utils/auth.js
@@ -0,0 +1,10 @@
+// utils/auth.js
+export const saveToken = (token) => localStorage.setItem('token', token);
+export const getToken = () => localStorage.getItem('token');
+export const removeToken = () => localStorage.removeItem('token');
+
+export const saveUsername = (username) => localStorage.setItem('username', username);
+export const getUsername = () => localStorage.getItem('username');
+export const removeUsername = () => localStorage.removeItem('username');
+
+export const isAuthenticated = () => !!getToken();
