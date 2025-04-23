0fff9a6210925a776fe225482311cebd614ed741
Kobby24 <geffah005@st.ug.edu.gh>
2025-04-23 22:22:49 +0000

    feat: add BlogGrid component for fetching and displaying posts with CSRF token handling

---
+++ src/data/usePosts.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const BlogGrid = () => {useEffect(() => {
+    axios.get(' https://print-gurus.onrender.com//all-posts/')
+      .then(response => {
+        return response.data;
+      })
+      .catch(error => {
+        console.error("Error fetching posts:", error);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios.get('https://print-gurus.onrender.com/csrf/')
+      .then(res => {
+        axios.defaults.headers.post['X-CSRFToken'] = res.data.csrfToken;
+      });
+  }, []);}
+
+  export default BlogGrid;
