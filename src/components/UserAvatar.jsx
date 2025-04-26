18d5bbd80754ee50103fb051b08f25db471224e7
Kobby24 <geffah005@st.ug.edu.gh>
2025-04-26 13:48:24 +0000

    feat: enhance UserAvatar component with customizable size and improved styling

--- src/components/UserAvatar.jsx
+++ src/components/UserAvatar.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import { Avatar } from '@mui/material';
 
-    function UserAvatar({ username }) {
-        const profileInitials = username ? username.charAt(0).toUpperCase() : '';
+function UserAvatar({ username, size = 40 }) {
+    const profileInitials = username ? username.charAt(0).toUpperCase() : '';
 
-        return (
-            <Avatar sx={{ bgcolor: 'primary.main' }}>
-                {profileInitials}
-            </Avatar>
-        );
-    }
-    
+    return (
+        <Avatar 
+            sx={{ 
+                background: 'linear-gradient(135deg,rgba(252, 252, 252, 0.75),rgba(241, 231, 244, 0.6))', // Notice: 'background' not 'bgcolor'
+                color: 'black',
+                fontWeight: 'bold',
+                fontSize: size / 2.5,
+                width: size,
+                height: size,
+                boxShadow: '0 4px 10px rgba(0,0,0,0.3)',
+                border: '2px solid white',
+            }}
+        >
+            {profileInitials}
+        </Avatar>
+    );
+}
+
 export default UserAvatar;
