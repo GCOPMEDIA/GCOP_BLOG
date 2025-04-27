07c44da8477e19e2248231e2a8362390d250e6ba
Kobby24 <geffah005@st.ug.edu.gh>
2025-04-27 16:25:25 +0000

    nn

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React,{useState,useEffect} from "react";
 import axios from "axios";
 import { Box, Typography,Container } from "@mui/material";
-import BlogGrid from "../components/Cards";
+import BlogGrid,{Slide} from "../components/Cards";
 import BlogList from "../components/data";
 import Background from "../components/Background";
 import Carousel from "../components/Carousel";
@@ -12,38 +12,10 @@ import Slider from "react-slick";
 
 
 
+
 const Home = () => {
-  const fallbackImages = [
-      { title: "Welcome to Church", url: "/images/church1.jpg" },
-      { title: "Join Our Services", url: "/images/church2.jpg" },
-      { title: "Be Part of Our Community", url: "/images/church3.jpg" }
-    ];
-    const [items, setItems] = useState([]);
-    useEffect(() => {
-        axios.get("http://print-gurus.onrender.com/events/")
-          .then((response) => {
-            const fetchedItems = response.data.data;
-            if (fetchedItems.length === 0) {
-              setItems(fallbackImages);
-            } else {
-              setItems(fetchedItems);
-            }
-          })
-          .catch((error) => {
-            console.error("Error fetching events:", error);
-            setItems(fallbackImages);
-          });
-      }, []);
-      const carouselSettings = {
-        dots: true,
-        infinite: true,
-        speed: 1000,
-        slidesToShow: 1,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        slidesToScroll: 1,
-        arrows: true
-      };
+ 
+    
 
   return (
     <main>
@@ -59,47 +31,7 @@ const Home = () => {
       {/* Carousel */}
       <Background children=
       <Container>
-      <Box sx={{ position: "relative", overflow: "hidden" }}>
-      <Slider {...carouselSettings}>
-        {items.map((item, index) => (
-          <Box
-            key={index}
-            sx={{
-              height: "70vh",
-              position: "relative",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              background: item.url.endsWith(".mp4")
-                ? "black"
-                : `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(${item.url}) center/cover no-repeat`
-            }}
-          >
-            {item.url.endsWith(".mp4") ? (
-              <video
-                src={item.url}
-                autoPlay
-                loop
-                muted
-                style={{ width: "100%", height: "100%", objectFit: "cover" }}
-              />
-            ) : null}
-            <Typography
-              variant="h3"
-              sx={{
-                position: "absolute",
-                color: "white",
-                fontWeight: "bold",
-                textAlign: "center",
-                px: 2
-              }}
-            >
-              {item.title}
-            </Typography>
-          </Box>
-        ))}
-      </Slider>
-    </Box>
+      <Slide />
       {/* <Box sx={{ position: "relative", overflow: "hidden" }}>
         <Slider {...carouselSettings}>
           {carouselImages.map((img, index) => (
